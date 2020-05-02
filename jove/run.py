00ce5c76432db683f3b56a9a44b66f5baef920e8
Jozef Vesely <vesely@gjh.sk>
2020-05-02 19:57:28 +0200

    make run_{python,gpu} methods of DataSet

--- jove/run.py
+++ jove/run.py
@@ -1,104 +1,5 @@
 #!/usr/bin/python3
 
-def run_python(dset, output, fit_size=3):
-    import time
-    from PIL import Image
-    import numpy as np
-    from scipy.signal import correlate, choose_conv_method
-
-    def subpixel_peak(d, s=1):
-        """Fit polynomial quadratic in x and y to the neighbourhood of maximum,
-           to determine maximum with subpixel precision"""
-        y, x = np.unravel_index(np.argmax(d[s:d.shape[0]-s,s:d.shape[1]-s]), (d.shape[0]-2*s, d.shape[1]-2*s))
-
-        if d[y+s, x+s] == d[d.shape[0]//2, d.shape[1]//2]:
-            # default to center, if not worse
-            y, x = d.shape[0]//2-s, d.shape[1]//2-s
-
-        #from pylab import matshow, show, plot
-        #matshow(d)
-        #plot([xp],[yp],"x")
-        #show()
-
-        l = d[y:y+2*s+1, x:x+2*s+1]
-        yi, xi = np.indices(l.shape)
-        i = np.ones(l.shape)
-        A = np.dstack([i, xi, yi, xi*xi, xi*yi, yi*yi]).reshape((-1,6))
-        b = l.reshape(-1)
-        #print(repr(b))
-        q = np.linalg.lstsq(A, b, rcond=None)[0]
-        #print(repr(q))
-        try:
-            xs, ys = np.linalg.solve([[2*q[3],   q[4]],
-                                      [  q[4], 2*q[5]]], [-q[1], -q[2]])
-        except np.linalg.LinAlgError:
-            xs, ys = s, s # if subpixel failed default to integer maximum
-        return (y+ys, x+xs), q
-
-    started = time.time()
-    s = dset.roi.size
-    ref = np.asarray(Image.open(dset.ref))
-    window = np.hanning(2*s)
-    ref_rois = []
-    for j,i in dset.roi.positions:
-        a = ref[i-s:i+s, j-s:j+s]
-        a = a - a.mean()
-        a *= window[:,None]
-        a *= window[None,:]
-        ref_rois.append(a)
-
-    method, times = choose_conv_method(ref_rois[0], ref_rois[0], mode='full', measure=True)
-    print("conv method", method, times)
-    with open(output, "w") as fh:
-        for x,y,fname in dset:
-            print(".", end="", flush=True)
-            fh.write("{:.6f} {:.6f} {} {}\n".format(x, y, len(dset.roi.positions), fname))
-            data = np.asarray(Image.open(fname))
-            for (j,i), a in zip(dset.roi.positions, ref_rois):
-                b = data[i-s:i+s, j-s:j+s]
-                b = b - b.mean()
-                b *= window[:,None]
-                b *= window[None,:]
-                cor = correlate(a, b, mode='full', method=method)
-                (yp, xp), q = subpixel_peak(cor, fit_size)
-                xp = xp-2*s+1
-                yp = yp-2*s+1
-                fh.write("{} {} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f}\n".format(j, i, xp, yp, *q))
-    print()
-    print(time.time()-started)
-
-
-def run_emida(dset, output, fit_size=3):
-    with open("emida-work.txt", "w") as fh:
-        for x, y, fn in dset: 
-            print(x, y, fn, file=fh)
-
-    dset.roi.save("emida-roi.txt")
-
-    isize = Image.open(dset.ref).size
-
-    c = 4*dset.roi.size-1
-    #c = 4*dset.roi.size//3-1
-
-    args = [
-        "../build/bin/emida",
-        "-d", "emida-work.txt",
-        "-b", "emida-roi.txt",
-        "-i", dset.ref,
-        "-p", "{},{}".format(*isize),
-        "-c", "{},{}".format(c,c),
-        "-f", "{}".format(2*fit_size+1),
-        "-q",
-    ]
-    print(" ".join(args))
-
-    import subprocess, time
-    started = time.time()
-    with open(output, "w") as fh:
-        subprocess.call(args, stdout=fh)
-
-    print(time.time()-started)
-
 from tools import HexDataSet, ROIs
 deformed = HexDataSet("../../Testing data/FeAl/DEFORMED_FeAl/DEFORMED_x{x:d}y{y:d}.tif", 7000, 60,
         ref="../../Testing data/FeAl/INITIAL_FeAl/INITIAL_x0y0.tif",
@@ -116,16 +17,16 @@ if __name__ == "__main__":
     #deformed.roi.plot()
     #show()
 
-    #run_emida(deformed.decimate(10), "out-deformed-emida-10.txt", fit_size=3)
-    run_python(deformed.decimate(10), "out-deformed-jove-10.txt", fit_size=3)
-    #run_emida(deformed.decimate(5), "out-deformed-emida-5.txt", fit_size=3)
-    #run_python(deformed.decimate(5), "out-deformed-jove-5.txt", fit_size=3)
-    #run_emida(deformed, "out-deformed-emida.txt", fit_size=3)
-    #run_python(deformed, "out-deformed-jove.txt", fit_size=3)
+    #deformed.decimate(10).run_gpu("out-deformed-emida-10.txt", fit_size=3)
+    deformed.decimate(10).run_python("out-deformed-jove-10.txt", fit_size=3)
+    #deformed.decimate(5).run_gpu("out-deformed-emida-5.txt", fit_size=3)
+    #deformed.decimate(5).run_python("out-deformed-jove-5.txt", fit_size=3)
+    #deformed.run_gpu("out-deformed-emida.txt", fit_size=3)
+    #deformed.run_python("out-deformed-jove.txt", fit_size=3)
 
-    #run_emida(initial.decimate(10), "out-initial-emida-10.txt", fit_size=3)
-    #run_python(initial.decimate(10), "out-initial-jove-10.txt", fit_size=3)
-    #run_emida(initial.decimate(5), "out-initial-emida-5.txt", fit_size=3)
-    #run_python(initial.decimate(5), "out-initial-jove-5.txt", fit_size=3)
-    #run_emida(initial, "out-initial-emida.txt", fit_size=3)
-    #run_python(initial, "out-initial-jove.txt", fit_size=3)
+    #initial.decimate(10).run_gpu("out-initial-emida-10.txt", fit_size=3)
+    #initial.decimate(10).run_python("out-initial-jove-10.txt", fit_size=3)
+    #initial.decimate(5).run_gpu("out-initial-emida-5.txt", fit_size=3)
+    #initial.decimate(5).run_python("out-initial-jove-5.txt", fit_size=3)
+    #initial.run_gpu("out-initial-emida.txt", fit_size=3)
+    #initial.run_python("out-initial-jove.txt", fit_size=3)
--- jove/tools.py
+++ jove/tools.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
+import numpy as np
+from PIL import Image
+
 def hexiter(size, step):
-    from math import sqrt
-    m = int(size/step/sqrt(0.75))
+    m = int(size/step/np.sqrt(0.75))
     n = int(size/step)
     for j in range(m+1):
         for i in range(n-j%2+1):
             x = (i+j%2/2)*step
-            y = j*sqrt(0.75)*step
+            y = j*np.sqrt(0.75)*step
             yield x, y
 
 
@@ -15,11 +17,10 @@ def hexplot(pos, C, step=None, cmap=None, vmin=None, vmax=None, ax=None, **kwarg
     if step is None:
         step = pos[1,0]-pos[0,0]
 
-    from numpy import array, sqrt
-    s3 = 1/sqrt(3)
-    hx = array([(0,  2*s3), ( 1,  s3), ( 1, -s3), 
-                (0, -2*s3), (-1, -s3), (-1,  s3)])*step/2
-    
+    s3 = 1/np.sqrt(3)
+    hx = np.array([(0,  2*s3), ( 1,  s3), ( 1, -s3),
+                   (0, -2*s3), (-1, -s3), (-1,  s3)])*step/2
+
     pos6 = pos[:,None,:] + hx[None,:,:]
 
     kwargs.setdefault('edgecolors', 'none')
@@ -51,9 +52,38 @@ def hexplot(pos, C, step=None, cmap=None, vmin=None, vmax=None, ax=None, **kwarg
     ax.set_aspect("equal")
     return col
 
+def subpixel_peak(d, s=1):
+    """Fit polynomial quadratic in x and y to the neighbourhood of maximum,
+       to determine maximum with subpixel precision"""
+    y, x = np.unravel_index(np.argmax(d[s:d.shape[0]-s,s:d.shape[1]-s]), (d.shape[0]-2*s, d.shape[1]-2*s))
 
-from numpy import savetxt, loadtxt, asarray, array
+    if d[y+s, x+s] == d[d.shape[0]//2, d.shape[1]//2]:
+        # default to center, if not worse
+        y, x = d.shape[0]//2-s, d.shape[1]//2-s
 
+    #from pylab import matshow, show, plot
+    #matshow(d)
+    #plot([xp],[yp],"x")
+    #show()
+
+    l = d[y:y+2*s+1, x:x+2*s+1]
+    yi, xi = np.indices(l.shape)
+    i = np.ones(l.shape)
+    A = np.dstack([i, xi, yi, xi*xi, xi*yi, yi*yi]).reshape((-1,6))
+    b = l.reshape(-1)
+    #print(repr(b))
+    q = np.linalg.lstsq(A, b, rcond=None)[0]
+    #print(repr(q))
+    try:
+        xs, ys = np.linalg.solve([[2*q[3],   q[4]],
+                                  [  q[4], 2*q[5]]], [-q[1], -q[2]])
+    except np.linalg.LinAlgError:
+        xs, ys = s, s # if subpixel failed default to integer maximum
+
+    return (y+ys, x+xs), q
+
+
+from scipy.signal import choose_conv_method, correlate
 class DataSet:
     def __init__(self, fmt, size, step, ang=None, ref=None, roi=None):
         self.fmt = fmt
@@ -63,7 +93,7 @@ class DataSet:
         self.ref = ref
         self.roi = roi
 
-        self.pos = array([ (x,y) for x, y, _ in self])
+        self.pos = np.array([ (x,y) for x, y, _ in self])
         self.fnames = [f for _, _, f in self]
 
     def decimate(self, n):
@@ -72,6 +102,79 @@ class DataSet:
     def __iter__(self):
         for x, y in self.iter(self.size, self.step):
             yield x/100, y/100, self.fmt.format(x=int(x), y=int(y))
+
+    def roi_iter(self, data):
+        s = self.roi.size
+        for j,i in self.roi.positions:
+            r = data[i-s:i+s, j-s:j+s]
+            r = r - r.mean()
+            r *= self.window[:,None]
+            r *= self.window[None,:]
+            yield r
+
+    def get_ref(self):
+        s = self.roi.size
+        self.window = np.hanning(2*s)
+
+        ref = np.asarray(Image.open(self.ref))
+        self.ref_rois = list( self.roi_iter(ref) )
+
+        self.conv_method, times = choose_conv_method(self.ref_rois[0], self.ref_rois[0], mode='full', measure=True)
+        print("conv method", self.conv_method, times)
+
+    def get_cor(self, fname, fit_size=3):
+        s = self.roi.size
+        data = np.asarray(Image.open(fname))
+        for a, b in zip(self.ref_rois, self.roi_iter(data)):
+            cor = correlate(a, b, mode='full', method=self.conv_method)
+            (yp, xp), q = subpixel_peak(cor, fit_size)
+            xp = xp-2*s+1
+            yp = yp-2*s+1
+            yield cor, xp, yp, q
+
+    def run_python(self, output, fit_size=3):
+        import time
+        started = time.time()
+        self.get_ref()
+        with open(output, "w") as fh:
+            for x, y, fname in self:
+                print(".", end="", flush=True)
+                fh.write("{:.6f} {:.6f} {} {}\n".format(x, y, len(self.roi.positions), fname))
+                for (j,i), (cor, xp, yp, q) in zip(self.roi.positions, self.get_cor(fname, fit_size=fit_size)):
+                    fh.write("{} {} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f}\n".format(j, i, xp, yp, *q))
+        print()
+        print(time.time()-started)
+
+    def run_gpu(dset, output, fit_size=3):
+        with open("emida-work.txt", "w") as fh:
+            for x, y, fn in dset:
+                print(x, y, fn, file=fh)
+
+        dset.roi.save("emida-roi.txt")
+
+        isize = Image.open(dset.ref).size
+
+        c = 4*dset.roi.size-1
+        #c = 4*dset.roi.size//3-1
+
+        args = [
+            "../build/bin/emida",
+            "-d", "emida-work.txt",
+            "-b", "emida-roi.txt",
+            "-i", dset.ref,
+            "-p", "{},{}".format(*isize),
+            "-c", "{},{}".format(c,c),
+            "-f", "{}".format(2*fit_size+1),
+            "-q",
+        ]
+        print(" ".join(args))
+
+        import subprocess, time
+        started = time.time()
+        with open(output, "w") as fh:
+            subprocess.call(args, stdout=fh)
+
+        print(time.time()-started)
 
 
 class HexDataSet(DataSet):
@@ -88,13 +191,13 @@ class ROIs:
     def load(cls, fname):
         with open(fname) as fh:
             size = int(fh.readline())
-            positions = loadtxt(fh,  dtype=int, ndmin=2)
+            positions = np.loadtxt(fh,  dtype=int, ndmin=2)
         return cls(size, positions)
 
     def save(self, fname):
         with open(fname, "w") as fh:
             print(self.size, file=fh)
-            savetxt(fh, self.positions, fmt="%d")
+            np.savetxt(fh, self.positions, fmt="%d")
 
     def plot(self, ax=None):
         from matplotlib.patches import Rectangle
@@ -111,9 +214,9 @@ class ROIs:
 
     @classmethod
     def regular(cls, size, step, mask):
-        positions = [ (i,j) for i in range(size, mask.shape[0]-size+1, step) 
-                            for j in range(size, mask.shape[1]-size+1, step) 
-                            if mask[i-size:i+size, 
+        positions = [ (i,j) for i in range(size, mask.shape[0]-size+1, step)
+                            for j in range(size, mask.shape[1]-size+1, step)
+                            if mask[i-size:i+size,
                                     j-size:j+size].all() ]
         return cls(size, positions)
 
@@ -133,9 +236,9 @@ def load_result(fname):
             x, y, n = float(x), float(y), int(n)
             pos.append((x,y))
             fnames.append(fn)
-            data.append(loadtxt(fh, max_rows=n))
-    pos = asarray(pos)
-    data = asarray(data)
+            data.append(np.loadtxt(fh, max_rows=n))
+    pos = np.asarray(pos)
+    data = np.asarray(data)
     print("loaded in", time.time()-started, "s")
     return pos, fnames, data
 
@@ -144,15 +247,13 @@ if __name__ == "__main__":
     data = HexDataSet("../../Testing data/FeAl/DEFORMED_FeAl/DEFORMED_x{x:d}y{y:d}.tif", 7000, 60)
     for x,y,f in data.decimate(20):
         print(x,y,f)
- 
-    from pylab import array, poisson, exp, show
-    val = poisson(500*exp( -((data.pos-35)**2).sum(axis=-1)/15/15/2))
+
+    from matplotlib.pyplot import imshow, show
+    val = np.random.poisson(500*np.exp( -((data.pos-35)**2).sum(axis=-1)/15/15/2))
     data.plot(data.pos, val)
     show()
 
-    from PIL import Image
-    from pylab import asarray, imshow, show
-    mask = asarray(Image.open("mask.png"))==255
+    mask = np.asarray(Image.open("mask.png"))==255
     imshow(mask)
     rois = ROIs.regular(48, 64, mask)
     #rois = ROIs.load("roi-cryst.txt")
