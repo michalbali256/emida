6eb15aa12451783d91d9fd2fc5572b2ed5a5d99f
Michal Bali <michalbali32@gmail.com>
2020-05-11 21:49:55 +0200

    python details

--- python/fft.py
+++ python/fft.py
@@ -4,8 +4,13 @@ import numpy as np
 import numpy.linalg
 import numpy.fft
 from scipy.signal import correlate
+import cmath
 
 np.set_printoptions(suppress=True,linewidth=np.nan)
+
+sh = [cmath.rect(1,2*cmath.pi/8*i*5) for i in range(0,5)]
+sh = [math.cos(2*cmath.pi/8*i*5) + math.sin(2*cmath.pi/8*i*5)*1j for i in range(0,5)]
+sy =np.array([cmath.rect(1,2*cmath.pi/8*i*5) for i in range(0,8)])
 
 a = [[1,2,3,1],[4,5,6,1],[7,8,9,1],[7,8,9,1]]
 b = [[6,3,2,1],[12,23,3,1],[1,8,9,1],[7,8,9,1]]
@@ -13,7 +18,7 @@ b = [[6,3,2,1],[12,23,3,1],[1,8,9,1],[7,8,9,1]]
 a_and_zeros = [[1,2,3,1,0,0,0,0],[4,5,6,1,0,0,0,0],[7,8,9,1,0,0,0,0],[7,8,9,1,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0]]
 b_and_zeros = [[6,3,2,1,0,0,0,0],[12,23,3,1,0,0,0,0],[1,8,9,1,0,0,0,0],[7,8,9,1,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0]]
 
-print(a_and_zeros)
+#print(a_and_zeros)
 #a_and_zeros = a.copy()
 #a_and_zeros.extend([0] * len(a))
 
@@ -25,10 +30,13 @@ ffta = np.fft.rfft2(a_and_zeros)
 fftb = np.fft.rfft2(b_and_zeros)
 print(ffta)
 print()
-print(fftb)
-print()
-print(ffta * np.conj(fftb))
-ia = np.fft.irfft2(ffta * np.conj(fftb))
+
+#print(fftb)
+#print()
+mult = ffta * np.conj(fftb)
+mult *= sh
+mult *= sy[:, np.newaxis]
+ia = np.fft.irfft2(mult)
 print(ia)
 
 cor = correlate(a, b, mode='full')
