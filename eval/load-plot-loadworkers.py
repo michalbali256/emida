31f1f56ff2f1081e446917a86e336b8be868b6a4
Michal Bali <michalbali32@gmail.com>
2021-01-03 20:57:14 +0100

    loadworkers figures

--- eval/load-plot-loadworkers.py
+++ eval/load-plot-loadworkers.py
@@ -8,38 +8,52 @@ part = sys.argv[1]
 #with open("out-graph-load-tiff-float.json","r") as fh:
 with open("out-graph-loadworkers-float.json","r") as fh:
     dataf = json.load(fh)
-with open("out-graph-loadworkers-double.json","r") as fh:
-    data = json.load(fh)
+#with open("out-graph-loadworkers-double.json","r") as fh:
+#    data = json.load(fh)
 
-fig, ax = plt.subplots(figsize=(12,6))
+parrts = ["Load tiff", "Offset", "Offset wait"]
+titles = ["The average time needed to load one batch of images for\ndifferent number of load workers (float, batch = 7)",
+    "Offset",
+    "The average time that the GPU stage of pipeline\nwaits for next loaded pattern (float, batch = 7)"]
+fnames = ["loadworkers-load-tiff.pdf", "loadworkers-offset.pdf", "loadworkers-offset-wait.pdf"]
+
+sizes = [(20,20),
+(20,80),
+(50,20),
+(50,50),
+(50,80),
+(80,50),
+(80,110)]
+
+
+
 
 data = dataf
 lab = ["double", "float"]
 rend = 80
-for x in range(1):
-    for roi_size in range(20, 110, 30):
-        for size in range(20, 110, 30):
-            xdata = []
-            ydata = []
-            for lw in data:
-                parts = data[lw][str(roi_size)][str(size)]
-                xdata.append(lw)
-                ydata.append(parts[part]["mean"])
-            ax.plot(xdata, ydata, label=lab[x] + ', size = {a}, S = {S}'.format(S=roi_size, a=size))
+for g in range(3):
+    fig, ax = plt.subplots(figsize=(8,4))
+    for s in sizes:
+        size, roi_size = s
+        xdata = []
+        ydata = []
+        for lw in data:
+            parts = data[lw][str(roi_size)][str(size)]
+            xdata.append(lw)
+            ydata.append(parts[parrts[g]]["mean"])
+        ax.plot(xdata, ydata, label='size = {a}, S = {S}'.format(S=roi_size, a=size))
     data = dataf
-#print(xdata)
-#print(ydata)
-# plot the data
+    ax.set_ylim(ymin=0)
+    
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.75, box.height])
 
-#ax.bar(xdata, ydata, color='tab:blue') #yerr=yerr)
-ax.set_ylim(ymin=0)
+    ax.set_title(titles[g])
+    ax.set_xlabel('Number of load workers')
+    ax.set_ylabel('time (ms)')
+        
 
-
-ax.set_title('Time that the thread computing offsets waits for loading of TIFF')
-ax.set_xlabel('size of subregion')
-ax.set_ylabel('time (ms)')
-plt.legend()
-# display the plot
-plt.show()
-fig.tight_layout()
-fig.savefig('load-plot-loadworkers.pdf')
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    # display the plot
+    plt.show()
+    fig.savefig(fnames[g])
---
+++ eval/load-plot-table.py
@@ -0,0 +1,27 @@
+import numpy as np
+import json
+import sys
+
+with open("out-graph-loadworkers-float.json","r") as fh:
+    data = json.load(fh)
+
+data = data["7"]
+
+tiff_size = 873*873
+tiff_bytes = tiff_size*2
+batch_size = tiff_bytes * 7
+batch_size_mb = batch_size / (1024*1024)
+
+print('S\\siz', end="&")
+for x in range(20, 112, 30):
+    print(x, end="&") 
+print()
+for roi_size in range(20, 112, 30):
+    print(roi_size, end='&')
+    for size in range(20, 112, 30):
+        parts = data[str(roi_size)][str(size)]
+        milliseconds = parts["Offset"]["mean"]
+        seconds = parts["Offset"]["mean"] / 1000
+        throughput_mb_s = batch_size_mb / seconds
+        print("{:.1f}".format(throughput_mb_s), "&", sep='', end='')
+    print()
